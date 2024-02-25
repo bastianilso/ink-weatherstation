88ebbb9985518adc130200d3152c4b5194ba0d4f
Bastian Ilso <contact@bastianilso.com>
2024-02-25 17:34:19 +0100

    Calendar: Show events for today and tomorrow

--- lib/calendar.py
+++ lib/calendar.py
@@ -23,18 +23,27 @@ def FetchTodaysProgram(calendars):
         print(c.name)
         if c.name == "SandraBastianKalender":
             cal = c
-    events_fetched = cal.date_search(start=date.today(), end=date.today() + timedelta(days=1), expand=True)
+    events_fetched = cal.date_search(start=date.today(), end=date.today() + timedelta(days=2), expand=True)
     for e in events_fetched:
         print(e.vobject_instance.vevent.summary.value)
-    e_val = [" " for i in range(6)]
+    en_val = [" " for i in range(6)]
+    ed_val = [" " for i in range(6)]
 
     for i in range(len(events_fetched)):
         print(events_fetched[i].vobject_instance.vevent.summary.value)
-        e_val[i] = events_fetched[i].vobject_instance.vevent.summary.value
+        name = events_fetched[i].vobject_instance.vevent.summary.value
+        en_val[i] = (name[:24] + '..') if len(name) > 24 else name
+        ed_date = events_fetched[i].vobject_instance.vevent.dtstart.value
+        if ed_date < date.today():
+            ed_date = date.today()
+        ed_val[i] = ed_date.strftime("%d/%m")
 
     if len(events_fetched) == 0:
-        e_val[0] = "ingen planer."
-    return(e_val)
+        en_val[0] = "ingen planer."
+        ed_val[0] = date.today().strftime("%d/%m")
+    
+    
+    return(en_val,ed_val)
 
 def FetchMonthBirthdays(calendars):
     cal = calendars[0]
@@ -69,12 +78,16 @@ def CalendarUpdate(tree):
     my_principal = client.principal()
     calendars = my_principal.calendars()
     
-    cal_events = FetchTodaysProgram(calendars)
+    cal_events, cal_dates = FetchTodaysProgram(calendars)
     for i in range(5):
         print(cal_events[i])
-        c_id = "$c" + str(i)
-        print(c_id)
-        SetEvent(tree, c_id, cal_events[i])
+        cn_id = "$cn" + str(i)
+        cd_id = "$cd" + str(i)
+        print(cd_id)
+        print(cn_id)
+        cd_id = "$cd" + str(i)
+        SetEvent(tree, cd_id, cal_dates[i])
+        SetEvent(tree, cn_id, cal_events[i])
 
     
     b_names, b_dates = FetchMonthBirthdays(calendars)
